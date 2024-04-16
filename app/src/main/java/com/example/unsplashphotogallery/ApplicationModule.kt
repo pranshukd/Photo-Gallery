f7d9d7e9eb6a8a0556f2ef0be79af6034ed3f2d5
pranshukd <pranshukumar906@gmail.com>
2024-04-17 01:11:15 +0530

    HILT imported + code working fine

---
+++ app/src/main/java/com/example/unsplashphotogallery/ApplicationModule.kt
@@ -0,0 +1,14 @@
+package com.example.unsplashphotogallery
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+
+//@Module
+//@InstallIn(SingletonComponent::class)
+object ApplicationModule {
+
+
+
+}
